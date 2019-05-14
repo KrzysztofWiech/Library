0f3f2b98a0e430a79eea4d9efc79e58b1d72f82e
KrzysztofWiech <bystrow7@gmail.com>
2019-05-14 21:13:29 +0200

    first commit

---
+++ src/data/Magazine.java
@@ -0,0 +1,73 @@
+package data;
+
+import java.util.Objects;
+
+public class Magazine extends Publication {
+    private int month;
+    private int day;
+    private String language;
+
+    public Magazine(String title, String publisher, String language, int year, int month, int day) {
+        super(year, title, publisher);
+        setLanguage(language);
+        setMonth(month);
+        setDay(day);
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder print = new StringBuilder(32);
+        print.append(getTitle());
+        print.append("; ");
+        print.append(getPublisher());
+        print.append("; ");
+        print.append(getYear());
+        print.append("; ");
+        print.append(getMonth());
+        print.append("; ");
+        print.append(getDay());
+        print.append("; ");
+        print.append(getLanguage());
+        return print.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Magazine)) return false;
+        if (!super.equals(o)) return false;
+        Magazine magazine = (Magazine) o;
+        return getMonth() == magazine.getMonth() &&
+                getDay() == magazine.getDay() &&
+                Objects.equals(getLanguage(), magazine.getLanguage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getMonth(), getDay(), getLanguage());
+    }
+}
---
+++ src/data/Publication.java
@@ -0,0 +1,54 @@
+package data;
+
+import java.util.Objects;
+
+public class Publication {
+    private int year;
+    private String title;
+    private String publisher;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    protected Publication(int year, String title, String publisher){
+        setYear(year);
+        setTitle((title));
+        setPublisher(publisher);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publication that = (Publication) o;
+        return year == that.year &&
+                Objects.equals(title, that.title) &&
+                Objects.equals(publisher, that.publisher);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, title, publisher);
+    }
+}
